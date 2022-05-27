1cfbcd0615e9ddda3749359e1acfb3124df8ecf2
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 14:07:20 -0700

    Created framework and detailed challenge in main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+Write a method with the n ame printDayOfTheWeek that has one parameter of type int and name day.
+The method should not return any value
+Using a switch statement print "Sunday" "Monday" etc if the int parameter day is 0,1,2 etc, otherwise say "Invalid day"
+
+Bonus! Write a second solution using if then else instead of switch.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+}
